6c3b5bf81e03db2f57f39390965e7568d93fbc78
Elysia <6873058@qq.com>
2023-12-24 11:00:33 +0800

    daily practice

---
+++ java-opp-package/src/com/yuanshen/StudentText.java
@@ -0,0 +1,12 @@
+package com.yuanshen;
+
+
+import com.yuanshen.domin1.Teacher;
+
+public class StudentText {
+    public static void main(String[] args) {
+        Teacher t=new Teacher();
+        com.yuanshen.domin2.Teacher t2=new com.yuanshen.domin2.Teacher();
+
+    }
+}
