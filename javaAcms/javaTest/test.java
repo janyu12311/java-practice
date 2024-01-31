1d648b99bfd1470119dff91eab16110ba0ddc9da
Elysia <6873058@qq.com>
2024-01-31 09:37:02 +0800

    daily practice

---
+++ javaAcms/javaTest/test.java
@@ -0,0 +1,11 @@
+package javaTest;
+
+public class test {
+
+    public static void main(String[] args) {
+        Long l1 = 1231111111111113231l;
+        Long l2 = 1231111111111113231l;
+        System.out.println(l1.hashCode());
+        System.out.println(l2.hashCode());
+    }
+}
